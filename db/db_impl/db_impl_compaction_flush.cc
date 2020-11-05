35bb5cc1cccb32d025e99d64aa0477051321b6bf
zhangxin <2452663915@qq.com>
2020-11-05 20:45:49 +0800

    fix archive problem

--- db/db_impl/db_impl_compaction_flush.cc
+++ db/db_impl/db_impl_compaction_flush.cc
@@ -2338,6 +2338,8 @@ void DBImpl::BackgroundCallCompaction(PrepickedCompaction* prepicked_compaction,
     Status s = env_->GetFileSize(log_name, &tmp_size);
     if (s.ok()) {
       global_wal_size += tmp_size;
+    } else {
+      fprintf(stdout, "fail to get size: %s.\n", log_name.c_str())
     }
   }
   ROCKS_LOG_BUFFER(
