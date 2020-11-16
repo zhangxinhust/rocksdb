73a16e60f1653c4bf8a754b5a1dd6ce34aab35e2
zhangxin <2452663915@qq.com>
2020-11-16 11:34:50 +0800

    add print

--- db/compaction/compaction_job.cc
+++ db/compaction/compaction_job.cc
@@ -1063,9 +1063,10 @@ void CompactionJob::ProcessKeyValueCompaction(SubcompactionState* sub_compact) {
     assert(sub_compact->builder != nullptr);
     assert(sub_compact->current_output() != nullptr);
     // zhangxin
-    fprintf(stdout, "&&&&& [%lu:%lu] before UpdateBoundaries.\n",
+    fprintf(stdout, "&&&&& [%lu:%lu], iter seq: %lu. before UpdateBoundaries.\n",
         sub_compact->current_output()->meta.fd.smallest_seqno, 
-        sub_compact->current_output()->meta.fd.largest_seqno);
+        sub_compact->current_output()->meta.fd.largest_seqno, 
+        c_iter->ikey().sequence);
 
     sub_compact->builder->Add(key, value);
     sub_compact->current_output_file_size = sub_compact->builder->FileSize();
