e1cf5778b1cf472b560d54a755a4e569ca6251f4
zhangxin <2452663915@qq.com>
2020-12-22 13:59:55 +0800

    fix segmentation fault

--- db/compaction/compaction_job.cc
+++ db/compaction/compaction_job.cc
@@ -841,10 +841,7 @@ Status CompactionJob::Install(const MutableCFOptions& mutable_cf_options) {
   }
 
   double wal_mb = 0;
-  std::vector<std::vector<double>> level_mb;
-  for (int i = 0; i < 5; i++) {
-    level_mb.push_back(std::vector<double>(7));
-  }
+  std::vector<std::vector<double>> level_mb(3, std::vector<double>(7, 0.0));
   for (auto _cfd : *versions_->GetColumnFamilySet()) {
     if (_cfd->IsDropped() || !_cfd->initialized() || _cfd->NumberLevels() < 1) {
       continue;
@@ -873,13 +870,13 @@ Status CompactionJob::Install(const MutableCFOptions& mutable_cf_options) {
     0, level_mb[0][0],
     1, level_mb[0][1],
     2, level_mb[0][2],
-    3, level_mb[0][2],
-    4, level_mb[0][2],
+    3, level_mb[0][3],
+    4, level_mb[0][4],
     0, level_mb[1][0],
     1, level_mb[1][1],
     2, level_mb[1][2],
-    3, level_mb[1][2],
-    4, level_mb[1][2]
+    3, level_mb[1][3],
+    4, level_mb[1][4]
   );
 
   uint32_t read_path_id, read_level_id;
